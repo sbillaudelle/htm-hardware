e13952504e9f75ca4090a4f842c8ad09049260b6
Sebastian Billaudelle <sbillaudelle@gmail.com>
2015-02-09 12:38:54 -0800

    Cleaned up spatial pooler

--- htm/spatial_pooler.py
+++ htm/spatial_pooler.py
@@ -1,74 +1,132 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gs
-import argparse
+import addict
 
 import pyNN.nest as pynn
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--columns', type=int, default=1000)
-parser.add_argument('--inputs', type=int, default=10000)
+class SpatialPooler(object):
+    """Timing based implementation of a spatial pooler"""
 
-parser.add_argument('--plot-histogram', action='store_true')
-parser.add_argument('--plot-traces', action='store_true')
+    # default parameters for the spatial pooler
+    defaults = addict.Dict()
 
-args = parser.parse_args()
+    defaults.config.timestep = 50.
+    defaults.config.input_size = 10000
+    defaults.config.record_traces = False
 
-pynn.setup(min_delay=0.01, timestep=0.01, threads=4)
+    defaults.populations.columns.size = 1000
+    defaults.populations.columns.neurons.e_rev_I = -65.0
+    defaults.populations.columns.neurons.v_thresh = -50.0
+    defaults.populations.columns.neurons.tau_m = 15.0
+    defaults.populations.columns.neurons.tau_refrac = 5.0
+    defaults.populations.columns.neurons.tau_syn_E = 6.0
+    defaults.populations.columns.neurons.tau_syn_I = 10.0
 
-# input populations
-connections = (np.random.uniform(0, 1, args.columns*args.inputs) > 0.80).reshape(args.columns, args.inputs).astype(np.int64)
-data = np.zeros(args.inputs)
-data[np.random.choice(np.arange(args.inputs), int(round(args.inputs*0.02)), replace=False)] = 1
+    defaults.populations.inhibition.neurons.e_rev_I = -80.0
+    defaults.populations.inhibition.neurons.v_reset = -70.0
+    defaults.populations.inhibition.neurons.v_thresh = -60.0
+    defaults.populations.inhibition.neurons.tau_m = 0.3
+    defaults.populations.inhibition.neurons.tau_refrac = 5.0
+    defaults.populations.inhibition.neurons.tau_syn_E = 1.0
+    defaults.populations.inhibition.neurons.tau_syn_I = 5.0
 
-activity = np.dot(connections, data)
-stimulus = pynn.Population(args.columns, pynn.SpikeSourceArray, {'spike_times': []})
-for i, s in enumerate(stimulus):
-    s.spike_times = np.sort(np.random.normal(1.0, 0.01, activity[i]))
+    defaults.projections.stimulus.weight = 0.002
+    defaults.projections.stimulus.jitter = 0.00001
+    defaults.projections.accumulation.weight = 0.015
+    defaults.projections.inhibition.weight = 0.3
+    defaults.projections.forward_inhibition.probability = 0.05
+    defaults.projections.forward_inhibition.weight = 0.00001
 
-# create compartments
-params_columns = {
-        'v_rest': -65.0,
-        'e_rev_E': 0.0,
-        'e_rev_I': -65.0,
-        'v_reset': -65.0,
-        'v_thresh': -50.0,
-        'tau_m': 15.0,
-        'tau_refrac': 5.0,
-        'tau_syn_E': 6.0,
-        'tau_syn_I': 10.0
-        }
-columns = pynn.Population(args.columns, pynn.IF_cond_exp, params_columns, structure=pynn.space.Line())
+    def __init__(self, params={}):
+        # merge parameters into defaults
+        self.parameters = self.defaults.copy()
+        self.parameters.update(params)
 
-params_kill_switch = {
-        'v_rest': -65.0,
-        'e_rev_E': 0.0,
-        'e_rev_I': -80.0,
-        'v_reset': -70.0,
-        'v_thresh': -60.0,
-        'tau_m': 0.3,
-        'tau_refrac': 5.0,
-        'tau_syn_E': 1.0,
-        'tau_syn_I': 5.0
-        }
-kill_switch = pynn.Population(1, pynn.IF_cond_exp, params_kill_switch)
+        # setup network
+        self.create()
+        self.connect()
 
-# connect populations
-pynn.Projection(stimulus, columns, pynn.OneToOneConnector(weights=0.002 + np.random.normal(0, 0.00001, args.columns)))
-pynn.Projection(columns, kill_switch, pynn.AllToAllConnector(weights=0.015))
-pynn.Projection(kill_switch, columns, pynn.AllToAllConnector(weights=0.3), target='inhibitory')
-pynn.Projection(stimulus, columns, pynn.FixedProbabilityConnector(0.05, weights=0.00001), target='inhibitory')
+    def create(self):
+        """Create the neuron populations"""
 
-columns.record()
-if args.plot_traces:
-    columns.record_v()
-    kill_switch.record_v()
+        n_columns = self.parameters.populations.columns.size
+        self.stimulus = pynn.Population(n_columns, pynn.SpikeSourceArray)
 
-pynn.run(50.)
+        # create compartments
+        self.columns = pynn.Population(n_columns, pynn.IF_cond_exp, self.parameters.populations.columns.neurons)
+        self.kill_switch = pynn.Population(1, pynn.IF_cond_exp, self.parameters.populations.inhibition.neurons)
+       
+        self.stimulus.record()
+        self.columns.record()
+        if self.parameters.config.record_traces:
+            self.columns.record_v()
+            self.kill_switch.record_v()
 
-spikes = columns.getSpikes()
-active = np.unique(spikes[:,0]).astype(np.int32)
+    def connect(self):
+        """Setup connections between populations"""
 
+        # connect populations
+        params = self.parameters.projections
+
+        stimulus_connector = pynn.OneToOneConnector(weights=params.stimulus.weight + np.random.normal(0, params.stimulus.jitter, len(self.columns)))
+        pynn.Projection(self.stimulus, self.columns, stimulus_connector)
+
+        accumulation_connector = pynn.AllToAllConnector(weights=params.accumulation.weight)
+        pynn.Projection(self.columns, self.kill_switch, accumulation_connector)
+
+        inhibition_connector = pynn.AllToAllConnector(weights=params.inhibition.weight)
+        pynn.Projection(self.kill_switch, self.columns, inhibition_connector, target='inhibitory')
+
+        forward_inhibition_connector = pynn.FixedProbabilityConnector(params.forward_inhibition.probability, weights=params.forward_inhibition.weight)
+        pynn.Projection(self.stimulus, self.columns, forward_inhibition_connector, target='inhibitory')
+        
+        n_columns = self.parameters.populations.columns.size
+        n_inputs = self.parameters.config.input_size
+    
+        self.connections = (np.random.uniform(0, 1, n_columns*n_inputs) > 0.80).reshape(len(self.columns), n_inputs).astype(np.int64)
+
+    def compute(self, data):
+        """Perform the actual computation"""
+
+        timestep = self.parameters.config.timestep
+
+        # generate spike train from given input vector data
+        train = np.ndarray((0, 2))
+        for i, d in enumerate(data):
+            activity = np.dot(self.connections, d)
+            for j in range(len(self.stimulus)):
+                spikes = np.sort(np.random.normal(1.0 + i*timestep, 0.01, activity[j]))
+                train = np.vstack([train, np.vstack([np.ones(spikes.size)*j, spikes]).T])
+
+        # set stimulus
+        for j, s in enumerate(self.stimulus):
+            s.spike_times = train[train[:,0] == j,1]
+
+        # run simulation
+        pynn.run(timestep*len(data))
+
+        # extract spikes and calculate activity
+        spikes = self.columns.getSpikes()
+        activity = []
+        for i in range(len(data)):
+            mask = (spikes[:,1] > i*timestep) & (spikes[:,1] < (i + 1)*timestep)
+            active = np.unique(spikes[mask,0]).astype(np.int32)
+            activity.append(active)
+        return activity
+
+    def get_spikes(self):
+        """Extract spike times from sources as well as columns"""
+
+        return (self.stimulus.getSpikes(), self.columns.getSpikes())
+
+    def get_traces(self):
+        """Extract traces from columns.
+        Requires setting config.record_traces
+        """
+
+        assert self.parameters.config.record_traces
+        return self.columns.get_v()
+
+"""
 print active.size
 open('asd.csv', 'a').write("{0:d}\n".format(active.size))
 
@@ -132,3 +190,4 @@ if args.plot_traces:
         ax.text(ax.get_xlim()[1] - 0.6, ax.get_ylim()[1] - 1, "{0}".format(activity[col]), va='top', ha='right')
 
     plt.savefig('spatial_pooler.pdf', bbox_inches='tight')
+"""
