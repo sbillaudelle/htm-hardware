ed649b9263052b609002622f73d4958ae6d86e68
Sebastian Billaudelle <sbillaudelle@gmail.com>
2015-03-24 15:46:16 -0700

    Added 4-input AdEx neuron model

--- htm/neuron_model.py
+++ htm/neuron_model.py
@@ -1,4 +1,7 @@
 import pyNN.nest as pynn
 
 pynn.nest.Install('brainscales_hw_module')
+pynn.nest.Install('hmf_hw_module')
+
 iaf_4_cond_exp = pynn.native_cell_type('iaf_4_cond_exp')
+aeif_4_cond_exp = pynn.native_cell_type('aeif_4_cond_exp')
